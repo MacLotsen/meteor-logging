c964e1f56af325636ea8af5767adce5f31b86199
Erik Nijenhuis <erik@xerdi.com>
2023-04-21 13:42:31 +0200

    Update versions

--- package.js
+++ package.js
@@ -1,15 +1,16 @@
 Package.describe({
     name: 'xerdi:logging',
-    version: '0.0.3',
+    version: '0.0.4',
     summary: 'Bundle of the ostrio logger packages',
     git: 'https://github.com/Xerdi/meteor-logging.git',
     documentation: 'README.md'
 });
 
 Package.onUse(function (api) {
+    api.versionsFrom('2.11.0');
     api.use([
-        'ecmascript@0.16.2',
         'meteor',
+        'ecmascript',
         'ostrio:logger@2.1.1',
         'ostrio:loggerfile@2.1.0',
         'ostrio:loggermongo@2.1.0',
